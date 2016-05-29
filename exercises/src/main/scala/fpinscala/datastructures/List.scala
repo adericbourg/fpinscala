3b5869475b11022b25e2ea0e816d6a91f906a852
Alban Dericbourg <alban@dericbourg.net>
2016-05-29 19:09:03 +0200

    Ex 3.12: reverse

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -94,5 +94,7 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def lengthLeft[A](l: List[A]): Int = foldLeft(l, 0)((acc, _) => acc + 1)
 
+  def reverse[A](l: List[A]) = foldLeft(l, Nil: List[A])((acc, item) => Cons(item, acc))
+
   def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
 }
